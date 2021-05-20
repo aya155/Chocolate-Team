c7afb5b693ddc5ce61ec542300c9d17e0d4fac7b
AliJalil93 <ali.abdulzahraa@gmail.com>
2021-05-20 19:05:34 +0300

    add Parcelable to classes

--- app/src/main/java/com/aya/chocolateteam/data/domain/City.kt
+++ app/src/main/java/com/aya/chocolateteam/data/domain/City.kt
@@ -1,8 +1,25 @@
 package com.aya.chocolateteam.data.domain
 
+import android.annotation.SuppressLint
+import android.os.Parcel
+import android.os.Parcelable
+
+@SuppressLint("ParcelCreator")
 data class City(
     val cityName: String,
     val countryName: String,
     val latitude: Double,
     val longitude: Double,
-    val population: Double)
+    val population: Double
+) : Parcelable {
+    override fun describeContents(): Int {
+        return  0
+    }
+
+
+    override fun writeToParcel(dest: Parcel?, flags: Int) {
+
+    }
+
+
+}
