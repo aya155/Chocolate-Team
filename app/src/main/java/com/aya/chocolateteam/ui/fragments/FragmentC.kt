099d4e80112537b5e21c6176b6dad2f4fad359df
Shahad Kadhim <almuhandisa.sh@gmail.com>
2021-05-25 22:35:56 +0300

    rf-search ui

--- app/src/main/java/com/aya/chocolateteam/ui/fragments/FragmentC.kt
+++ app/src/main/java/com/aya/chocolateteam/ui/fragments/FragmentC.kt
@@ -18,48 +18,48 @@ class FragmentC : BaseFragment<FragmentCBinding>() {
     override val bindingInflater: (LayoutInflater) -> FragmentCBinding = FragmentCBinding::inflate
    //set initial visibility
     override fun setup() {
-        setVisibility(true)
+//        setVisibility(true)
     }
     override fun addCallBack() {
-        binding!!.apply {
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            //click search icon in keyboard
-            override fun onQueryTextSubmit(query: String)=search()
-            //when change text and be empty set visibility
-            override fun onQueryTextChange(newText: String?):Boolean{
-                takeIf { searchView.query.isBlank() }?.let { setVisibility(true) }
-                return false
-            }
-        })
-
-            searchView.queryHint = "Search ..."
-        }
+//        binding!!.apply {
+//        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+//            //click search icon in keyboard
+//            override fun onQueryTextSubmit(query: String)=search()
+//            //when change text and be empty set visibility
+//            override fun onQueryTextChange(newText: String?):Boolean{
+//                takeIf { searchView.query.isBlank() }?.let { setVisibility(true) }
+//                return false
+//            }
+//        })
+//
+//            searchView.queryHint = "Search ..."
+//        }
     }
 
     private fun search():Boolean{
-        binding?.apply {
-            val country=DataManager.getCountryByName(searchView.query.toString())
-            // if country already exiting  in csv file
-            if (country!=null) {
-                val intent=Intent(activity,SearchResultActivity::class.java)
-                intent.putExtra(Constants.COUNTRY,country)
-                startActivity(intent)
-                setVisibility(true)
-            }// if invalid country
-            else setVisibility(false)
-        }
+//        binding?.apply {
+//            val country=DataManager.getCountryByName(searchView.query.toString())
+//            // if country already exiting  in csv file
+//            if (country!=null) {
+//                val intent=Intent(activity,SearchResultActivity::class.java)
+//                intent.putExtra(Constants.COUNTRY,country)
+//                startActivity(intent)
+//                setVisibility(true)
+//            }// if invalid country
+//            else setVisibility(false)
+//        }
         return false
     }
     private fun setVisibility(b:Boolean){
-        val searchVisible:Int=if(b)View.VISIBLE else View.INVISIBLE
-        val errorVisible:Int=if(b)View.INVISIBLE else View.VISIBLE
-        binding?.apply {
-            notFound.visibility=errorVisible
-            searchPhoto.visibility=searchVisible
-            subtext.visibility=searchVisible
-            text.visibility=searchVisible
-            errorText.visibility=errorVisible
-        }
+//        val searchVisible:Int=if(b)View.VISIBLE else View.INVISIBLE
+//        val errorVisible:Int=if(b)View.INVISIBLE else View.VISIBLE
+//        binding?.apply {
+//            notFound.visibility=errorVisible
+//            searchPhoto.visibility=searchVisible
+//            subtext.visibility=searchVisible
+//            text.visibility=searchVisible
+//            errorText.visibility=errorVisible
+//        }
     }
 
     override fun bindLayout(country: Country) {
