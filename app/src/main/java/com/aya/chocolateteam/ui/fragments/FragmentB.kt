9b332ea668be585083edc5fe1e24b5fb5ebe7482
AliJalil93 <ali.abdulzahraa@gmail.com>
2021-05-28 18:52:47 +0300

    repair map not appearing

--- app/src/main/java/com/aya/chocolateteam/ui/fragments/FragmentB.kt
+++ app/src/main/java/com/aya/chocolateteam/ui/fragments/FragmentB.kt
@@ -2,6 +2,7 @@ package com.aya.chocolateteam.ui.fragments
 
 
 import android.content.Intent
+import android.content.res.Resources
 import android.graphics.Color
 import android.graphics.Typeface
 import android.view.Gravity
@@ -21,12 +22,13 @@ import com.aya.chocolateteam.util.toFlagEmoji
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
 import kotlinx.android.synthetic.main.fragment_b.*
 import kotlinx.android.synthetic.main.fragment_c.*
 
 
-class FragmentB : BaseFragment<FragmentBBinding>(), OnMapReadyCallback {
+class FragmentB : BaseFragment<FragmentBBinding>(),OnMapReadyCallback {
     override val LOG_TAG: String = "FRAGMENT_B"
     override val bindingInflater: (LayoutInflater) -> FragmentBBinding = FragmentBBinding::inflate
 
@@ -36,7 +38,8 @@ class FragmentB : BaseFragment<FragmentBBinding>(), OnMapReadyCallback {
     lateinit var marker: Marker
 
 
-    private var gMap: GoogleMap? = null
+//    private var gMap: GoogleMap? = null
+    private lateinit var gMap: GoogleMap
 
     override fun setup() {
 //        Toast.makeText(activity, "setup", Toast.LENGTH_SHORT).show()
@@ -46,11 +49,14 @@ class FragmentB : BaseFragment<FragmentBBinding>(), OnMapReadyCallback {
     }
 
     fun mapInit() {
-        with(binding!!.mapView) {
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+
+        with(mapFragment) {
             // Initialise the MapView
             onCreate(null)
             // Set the map ready callback to receive the GoogleMap object
-            this.getMapAsync(this@FragmentB)
+            getMapAsync(this@FragmentB)
 //            this.getMapAsync {
 //                MapsInitializer.initialize(requireContext())
 //                setMapLocation(it)
