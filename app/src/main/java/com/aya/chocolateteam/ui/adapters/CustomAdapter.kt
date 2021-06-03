aedfcd9bded0187e9902467d72e536d2a6e48d0d
AliJalil93 <ali.abdulzahraa@gmail.com>
2021-06-03 18:58:48 +0300

    rf- CustomAdapter

--- app/src/main/java/com/aya/chocolateteam/ui/adapters/CustomAdapter.kt
+++ app/src/main/java/com/aya/chocolateteam/ui/adapters/CustomAdapter.kt
@@ -1,23 +1,42 @@
 package com.aya.chocolateteam.ui.adapters
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.aya.chocolateteam.R
 import com.aya.chocolateteam.data.domain.City
 import com.aya.chocolateteam.databinding.ItemBinding
 import com.aya.chocolateteam.databinding.ItemCityBinding
+import java.util.ArrayList
 
 
-class CustomAdapter(val cityList: List<City>) : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val binding= ItemCityBinding.bind(view)
+class CustomAdapter(
+    val cityList: List<City>,
+    val itemClick: (City) -> Unit
+) : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.item_city, parent, false),
+        itemClick
+    )
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindViewHolder(cityList[position])
     }
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)= ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_city, parent, false))
-    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        viewHolder.binding.cityName.text=cityList[position].cityName
-    }
+
     override fun getItemCount() = cityList.size
+
+
+    class ViewHolder(view: View, val itemClick: (City) -> Unit) : RecyclerView.ViewHolder(view) {
+        val binding = ItemCityBinding.bind(view)
+        var cityTxt = this.binding.cityName
+        fun bindViewHolder(cityItem: City) {
+            cityTxt.text = cityItem.cityName
+            itemView.setOnClickListener { itemClick(cityItem) }
+        }
+    }
 }
--- app/src/main/java/com/aya/chocolateteam/ui/fragments/FragmentC.kt
+++ app/src/main/java/com/aya/chocolateteam/ui/fragments/FragmentC.kt
@@ -1,5 +1,6 @@
 package com.aya.chocolateteam.ui.fragments
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.AdapterView
@@ -21,23 +22,29 @@ import kotlinx.android.synthetic.main.fragment_c.*
 
 
 class FragmentC : BaseFragment<FragmentCBinding>() {
-    override val LOG_TAG: String="FRAGMENT_C"
+    override val LOG_TAG: String = "FRAGMENT_C"
     lateinit var currentCountry: Country
-    private val errorViews= arrayListOf<View?>()
-    private val cityViews= arrayListOf<View?>()
-    private val countryViews= arrayListOf<View?>()
-    private val searchViews= arrayListOf<View?>()
+    private val errorViews = arrayListOf<View?>()
+    private val cityViews = arrayListOf<View?>()
+    private val countryViews = arrayListOf<View?>()
+    private val searchViews = arrayListOf<View?>()
     override val bindingInflater: (LayoutInflater) -> FragmentCBinding = FragmentCBinding::inflate
-   //set initial visibility
+
+    //set initial visibility
     override fun setup() {
-       initialViews()
-       setVisibility(searchViews,errorViews,cityViews,countryViews)
-   }
-    private fun setVisibility(listVisible :ArrayList<View?>, vararg viewInvisible :ArrayList<View?>){
-        viewInvisible.forEach {it.forEach {view-> view?.visibility=View.GONE } }
-        listVisible.forEach { it?.visibility=View.VISIBLE}
+        initialViews()
+        setVisibility(searchViews, errorViews, cityViews, countryViews)
     }
-    private fun initialViews(){
+
+    private fun setVisibility(
+        listVisible: ArrayList<View?>,
+        vararg viewInvisible: ArrayList<View?>
+    ) {
+        viewInvisible.forEach { it.forEach { view -> view?.visibility = View.GONE } }
+        listVisible.forEach { it?.visibility = View.VISIBLE }
+    }
+
+    private fun initialViews() {
         binding?.run {
             searchViews.apply {
                 add(mainText)
@@ -78,112 +85,184 @@ class FragmentC : BaseFragment<FragmentCBinding>() {
             }
         }
     }
+
     override fun addCallBack() {
 
         binding!!.apply {
             search.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
                 //click search icon in keyboard
-                override fun onQueryTextSubmit(query: String)=search(search.query.toString())
+                override fun onQueryTextSubmit(query: String) = search(search.query.toString())
+
                 //when change text and be empty set visibility
-                override fun onQueryTextChange(newText: String?):Boolean{
-                    takeIf { search.query.isBlank() }?.let { setVisibility(searchViews,errorViews,cityViews,countryViews) }
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    takeIf { search.query.isBlank() }?.let {
+                        setVisibility(
+                            searchViews,
+                            errorViews,
+                            cityViews,
+                            countryViews
+                        )
+                    }
                     return false
                 }
             })
             cityCountry.setOnClickListener {
                 searchCountry((it as TextView).text.toString())
-                search.setQuery(it .text.toString(),false)
+                search.setQuery(it.text.toString(), false)
             }
             countryCaptail.setOnClickListener {
                 searchCity((it as TextView).text.toString())
                 search.setQuery(it.text.toString(), false)
             }
             spinnerFilter.selected {
-                search.queryHint=when(it){
+                search.queryHint = when (it) {
                     0 -> "search city ..... "
                     1 -> "search country ..... "
                     else -> "search long ,lat ..... "
                 }
             }
             sortBySpinner.selected {
-                listCity.adapter=when(it){
-                    0 -> CustomAdapter(DataManager.getCountryCities(country = currentCountry,sortBy = SortBy.CityName,sortType = SortType.Ascending))
-                    1 ->CustomAdapter(DataManager.getCountryCities(country = currentCountry,sortBy = SortBy.Population))
-                    2 ->CustomAdapter(DataManager.getCountryCities(country = currentCountry,sortBy = SortBy.Latitude))
-                    else ->CustomAdapter(DataManager.getCountryCities(country = currentCountry,sortBy = SortBy.Longitude))
+                listCity.adapter = when (it) {
+                    0 -> CustomAdapter(
+                        DataManager.getCountryCities(
+                            country = currentCountry,
+                            sortBy = SortBy.CityName,
+                            sortType = SortType.Ascending
+                        )
+                    ) { cityModel ->
+                        log(cityModel.cityName)
+                    }
+                    1 -> CustomAdapter(
+                        DataManager.getCountryCities(
+                            country = currentCountry,
+                            sortBy = SortBy.Population
+                        )
+                    ) { cityModel ->
+                        log(cityModel.cityName)
+                    }
+                    2 -> CustomAdapter(
+                        DataManager.getCountryCities(
+                            country = currentCountry,
+                            sortBy = SortBy.Latitude
+                        )
+                    ) { cityModel ->
+                        log(cityModel.cityName)
+                    }
+                    else -> CustomAdapter(
+                        DataManager.getCountryCities(
+                            country = currentCountry,
+                            sortBy = SortBy.Longitude
+                        )
+                    ) { cityModel ->
+                        log(cityModel.cityName)
+                    }
                 }
             }
             btnGoToMap.setOnClickListener {
-                activity?.findViewById<ViewPager>(R.id.viewpager)?.currentItem=0
+                activity?.findViewById<ViewPager>(R.id.viewpager)?.currentItem = 0
             }
         }
     }
-    private fun search(searchText:String):Boolean{
+
+    private fun search(searchText: String): Boolean {
         binding?.apply {
-            when (spinnerFilter.selectedItem.toString()){
+            when (spinnerFilter.selectedItem.toString()) {
                 "City" -> searchCity(searchText)
-                "Country"-> searchCountry(searchText)
+                "Country" -> searchCountry(searchText)
                 "Location" -> searchLocation(searchText)
-                else ->searchCountry(searchText)
+                else -> searchCountry(searchText)
             }
         }
         return false
     }
-    private fun errorSearch(){ setVisibility(errorViews,searchViews,countryViews,cityViews) }
-    private fun searchCountry(countryName:String) {
-            val country= DataManager.getCountryByName(countryName)
-            // if country already exiting  in csv file
-            if (country!=null) {
-                currentCountry=country
-                setVisibility(countryViews,searchViews,errorViews,cityViews)
-                bindCountryLayout(country)
-            }else errorSearch()
+
+    private fun errorSearch() {
+        setVisibility(errorViews, searchViews, countryViews, cityViews)
     }
 
-    private fun searchLocation(location:String) {
+    private fun searchCountry(countryName: String) {
+        val country = DataManager.getCountryByName(countryName)
+        // if country already exiting  in csv file
+        if (country != null) {
+            currentCountry = country
+            setVisibility(countryViews, searchViews, errorViews, cityViews)
+            bindCountryLayout(country)
+        } else errorSearch()
+    }
 
-        val city :City?=DataManager.searchCityByLongLat(location.split(','))
+    private fun searchLocation(location: String) {
+
+        val city: City? = DataManager.searchCityByLongLat(location.split(','))
         // if city already exiting  in csv file
-        if (city!=null) {
-            setVisibility(cityViews,searchViews,errorViews,countryViews)
+        if (city != null) {
+            setVisibility(cityViews, searchViews, errorViews, countryViews)
             bindCityLayout(city)
-        }else errorSearch()
+        } else errorSearch()
     }
-    private fun searchCity(cityName:String) {
-        val city= DataManager.searchCityByName(cityName)
+
+    private fun searchCity(cityName: String) {
+        val city = DataManager.searchCityByName(cityName)
         // if city already exiting  in csv file
-        if (city!=null) {
-            setVisibility(cityViews,searchViews,errorViews,countryViews)
+        if (city != null) {
+            setVisibility(cityViews, searchViews, errorViews, countryViews)
             bindCityLayout(city)
-        }else errorSearch()
+        } else errorSearch()
     }
+
     private fun bindCityLayout(city: City) {
         binding?.apply {
-            cityPopulation.text= city.population.toString()
-            cityCountry.text=city.countryName
-            cityName.text=city.cityName
-            iso2.text=city.iso2
-            iso3.text=city.iso3
-            Constants.cityMap=city
+            cityPopulation.text = city.population.toString()
+            cityCountry.text = city.countryName
+            cityName.text = city.cityName
+            iso2.text = city.iso2
+            iso3.text = city.iso3
+            Constants.cityMap = city
         }
     }
+
     private fun bindCountryLayout(country: Country) {
         binding?.apply {
-            countryChart.aa_drawChartWithChartModel(bindChart(type = AAChartType.Bar,backgroundColor = "#d2b48c",title = country.name,seriesArray = makeSeriesArray(country.cities.shuffled().filter { it.population!=0.0 }.take(3)).toTypedArray()))
-            countryPopulation.text=DataManager.getTotalCountryPopulation(country).toString()
-            countryCaptail.text= DataManager.getCapitalCity(country)?.cityName
-            iso2.text=DataManager.getIso2ByCountry(country)
-            iso3.text=DataManager.getIso3ByCountry(country)
-            listCity.adapter=CustomAdapter(DataManager.getCountryCities(country = currentCountry,sortBy = SortBy.CityName,sortType = SortType.Ascending))
+            countryChart.aa_drawChartWithChartModel(
+                bindChart(
+                    type = AAChartType.Bar,
+                    backgroundColor = "#d2b48c",
+                    title = country.name,
+                    seriesArray = makeSeriesArray(
+                        country.cities.shuffled().filter { it.population != 0.0 }.take(3)
+                    ).toTypedArray()
+                )
+            )
+            countryPopulation.text = DataManager.getTotalCountryPopulation(country).toString()
+            countryCaptail.text = DataManager.getCapitalCity(country)?.cityName
+            iso2.text = DataManager.getIso2ByCountry(country)
+            iso3.text = DataManager.getIso3ByCountry(country)
+            listCity.adapter = CustomAdapter(
+                DataManager.getCountryCities(
+                    country = currentCountry,
+                    sortBy = SortBy.CityName,
+                    sortType = SortType.Ascending
+                )
+            ) { cityModel ->
+                log(cityModel.cityName)
+            }
+
+
         }
 
     }
+
     override fun bindLayout(country: Country) {
     }
-    private fun Spinner.selected(action: (position:Int) -> Unit) {
+
+    private fun Spinner.selected(action: (position: Int) -> Unit) {
         this.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {}
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
                 action(position)
             }
         }
